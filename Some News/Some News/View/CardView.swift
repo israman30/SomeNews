9812074d5396de90005d7f647f08da2bc2f2d1d4
Israel Manzo <israelmanzo814@gmail.com>
2025-05-05 14:17:11 -0400

    update card gradient color for display text

--- Some News/Some News/View/CardView.swift
+++ Some News/Some News/View/CardView.swift
@@ -12,34 +12,46 @@ struct CardView: View {
     var article: Articles
     
     var body: some View {
-        VStack {
+        ZStack {
             AsyncImage(url: URL(string: article.urlToImage ?? "")) { image in
                 image.image?.resizable()
                     .scaledToFit()
             }
-            VStack(alignment: .leading) {
-                Text(article.author ?? "")
-                    .font(.headline)
-                    .foregroundColor(.secondary)
-                Text(article.title ?? "no title")
-                    .font(.title)
-                    .foregroundColor(.primary)
-                    .fontWeight(.bold)
-                Text(article.description?.uppercased() ?? "")
-                    .font(.caption)
-                    .foregroundColor(.secondary)
+            .cornerRadius(10)
+            VStack {
+                Spacer()
+                VStack(alignment: .leading) {
+                    Text(article.author ?? "")
+                        .font(.headline)
+                    Text(article.title ?? "no title")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .lineLimit(2)
+                }
+                .padding(.horizontal, 5)
+                .frame(maxWidth: .infinity, maxHeight: 100)
+                .foregroundStyle(.white)
+                .background(
+                    LinearGradient(gradient: Gradient(colors: [.clear, .black]), startPoint: .top, endPoint: .bottom)
+                )
             }
         }
         .cornerRadius(10)
-        .overlay {
-            RoundedRectangle(cornerRadius: 10)
-                .stroke(Color(.sRGB, red: 150/255, green: 150/255, blue: 150/255, opacity: 0), lineWidth: 1)
-        }
     }
 }
 
 #Preview {
-    CardView(article: Articles(author: "Someone", title: "Somet Title", description: "This is the place for the description", url: "", urlToImage: "https://www.kbb.com/wp-content/uploads/2022/08/2022-mercedes-amg-eqs-front-left-3qtr.jpg?w=918", publishedAt: "12/20/23"))
+    CardView(
+        article: Articles(
+            author: "Someone",
+            title: "Somet Title",
+            description: "This is the place for the description, for the body of the card where should be more text.",
+            url: "",
+            urlToImage: "https://www.kbb.com/wp-content/uploads/2022/08/2022-mercedes-amg-eqs-front-left-3qtr.jpg?w=918",
+            publishedAt: "12/20/23"
+        )
+    )
+    .frame(height: 220)
 }
 
 
