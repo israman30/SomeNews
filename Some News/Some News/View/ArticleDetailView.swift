2a3fba27ed07308a69408e64ce21d73622f7989b
Israel Manzo <israelmanzo814@gmail.com>
2025-06-28 00:02:04 -0400

    updated articles detail

--- Some News/Some News/View/ArticleDetailView.swift
+++ Some News/Some News/View/ArticleDetailView.swift
@@ -82,11 +82,6 @@ struct ArticleDetailView: View {
                     }
                 }
                 .padding(20)
-                .background(
-                    RoundedRectangle(cornerRadius: 16)
-                        .fill(Color(.systemBackground))
-                        .shadow(color: Color.black.opacity(0.08), radius: 8, x: 0, y: 4)
-                )
                 .padding(.horizontal)
                 .padding(.bottom, 24)
             }
